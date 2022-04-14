3ac75a889ecd1bd4820f2367625d2dcef98f9d8e
Sudhar4107 <90951244+Sudhar4107@users.noreply.github.com>
2022-04-14 17:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from unittest import result
 from flask import Flask, render_template, request
 import joblib
-#C:\Users\ELCOT\Desktop\VsCode_Practice\deployment_env
 
 #initialize app
 app = Flask(__name__)
@@ -9,10 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('form.html')
-
-@app.route('/home')
-def home():
-    return render_template('index.html')
 
 @app.route('/submit', methods=["POST"])
 def Form_Submit():
@@ -33,10 +27,6 @@ def Form_Submit():
 
     return render_template('predict.html',data=f'Prediction result is {F}')
 
-
-@app.route('/contacts')
-def contact():
-    return('Contact Page!')
 
 if(__name__=='__main__'):
     app.run(debug= True)
